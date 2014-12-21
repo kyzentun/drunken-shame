b808bdc7c8cc01af4c5bc004fd876891705c0db6
Kyzentun <kyzentun@gmail.com>
2014-12-21 01:35:34 -0700

    Kyouko-chan is best girl.

--- BGAnimations/ScreenGameplay in.lua
+++ BGAnimations/ScreenGameplay in.lua
@@ -1,10 +1,12 @@
 return Def.ActorFrame{
 	Def.Actor{
-		Name= "Mari", StartTransitioningCommand= function(self)
+		Name= "Tomoko", StartTransitioningCommand= function(self)
 			local scr= SCREENMAN:GetTopScreen()
-			scr:linear(4)
-			scr:zoom(0)
 			scr:xy(_screen.cx, _screen.cy)
+			scr:zoom(0)
+			scr:linear(4)
+			scr:zoom(1)
+			scr:xy(0, 0)
 			self:sleep(4)
 		end
 	}
